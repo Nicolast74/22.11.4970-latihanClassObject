2a37a40db8e9dfd88f78ae78f5c5e3e0a5a2f784
Nicolast74 <nicolast.nicolast.74@gmail.com>
2023-03-21 21:33:03 +0700

    LatihanClassObject

---
+++ LatihanClassObject-master/Class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxiDriver
+{
+    public class Class
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drive Name: {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Number Of Passanger: {0}", NumPassanger);
+        }
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("\nPak Supri sedang menjemput penumpang");
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("Pak Supri selesai mengantar penumpang");
+        }
+    }
+}
---
+++ LatihanClassObject-master/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxiDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Class taxi = new Class();
+
+            // pengertian nilai properties
+            taxi.DriverName = "Pak Supri";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 25;
+
+            // pemanggil method
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
